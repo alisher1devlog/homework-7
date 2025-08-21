ab1fa70d870d62cc7d284364e3679de32171bc5f
Alisher <alisheryondoshaliyev77@gmail.com>
2025-08-21 21:23:41 +0500

    fa

---
+++ drinkSorting.js
@@ -0,0 +1,18 @@
+/*Drink Sorting
+You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+Assume that the following array of drink objects needs to be sorted:
+
+Array ichida object bo'lsin objectlarni price bo'yicha sort qiladigan algorithm tuzing!
+drinks = [
+  {name: "lemonade", price: 50},
+  {name: "lime", price: 10}
+] 
+*/
+
+drinks = [
+  {name: "lemonade", price: 50},
+  {name: "lime", price: 10}
+] 
+
+console.log("45" + 50);
